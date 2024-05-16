8d20a1837d1345f8ee7dce6223fe26225008bf91
Mahfooz Abubakar <mahfooz.abubakar@wemabank.com>
2024-05-16 07:03:53 +0100

    Added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'c721ps',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "http://universities.hipolabs.com"
   },
